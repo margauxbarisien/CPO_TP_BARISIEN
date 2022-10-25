acd3c4ecb5a10abc06461bf319df2660faf60c0c
marga <margaux.barisien@epfedu.fr>
2022-10-25 16:47:49 +0200

    voilà les 5 bières

--- TP2_Bières_BARISIEN/src/tp2_bières_barisien/TP2_Bières_BARISIEN.java
+++ TP2_Bières_BARISIEN/src/tp2_bières_barisien/TP2_Bières_BARISIEN.java
@@ -13,7 +13,25 @@ public class TP2_Bières_BARISIEN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson") ;
+        //uneBiere.Nom = "Cuvee des trolls";
+        //uneBiere.degreAlcool = 7.0 ;
+        //uneBiere.brasserie = "Dubuisson";
+        uneBiere.ouverte = false ;       
+        uneBiere.lireEtiquette();
     
+        BouteilleBiere BiereLeffe = new BouteilleBiere("Leffe", 6.6, "Abbaye de Leffe") ;
+        BiereLeffe.lireEtiquette();
+        
+        BouteilleBiere BiereChouffe = new BouteilleBiere("Chouffe", 8, "Chez Andrea") ;
+        BiereChouffe.lireEtiquette();
+        
+        BouteilleBiere BiereGoudale = new BouteilleBiere("Goudale", 7.2, "TirBouchon") ;            
+        BiereGoudale.lireEtiquette();
+        
+        BouteilleBiere BiereHeineken = new BouteilleBiere("Heineken", 5, "neuneu") ;
+        BiereHeineken.lireEtiquette();
+  
+}
 }

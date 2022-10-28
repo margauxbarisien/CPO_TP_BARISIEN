819b487a0eb9dc79568edd482c34f1bc517b2656
marga <margaux.barisien@epfedu.fr>
2022-10-28 14:56:02 +0200

    Commentaires + quelques bières décapsulées (+ affichage de leurs caractéristiques de plusieurs manières différentes)

--- TP2_Bières_BARISIEN/src/tp2_bières_barisien/BouteilleBiere.java
+++ TP2_Bières_BARISIEN/src/tp2_bières_barisien/BouteilleBiere.java
@@ -9,47 +9,47 @@ package tp2_bières_barisien;
  * @author marga
  */
 public class BouteilleBiere {
-    String Nom;
+    String Nom; //création des caractéristiques de la bière avec les types
     double degreAlcool;
     String brasserie;
     boolean ouverte;
 
-    public void lireEtiquette() {
+    public void lireEtiquette() { //fonction qui lit l'étiquette de la bière
     
     System.out.println("Bouteille de " + Nom +" (" + degreAlcool +
-    " degres) \nBrasserie : " + brasserie + "\nouverte : " + ouverte) ;
+    " degres) \nBrasserie : " + brasserie + "\nouverte : " + ouverte) ; //les \n permettent d'aller à la ligne
     }
     public BouteilleBiere(String unNom, double unDegre, String 
-    uneBrasserie, boolean ouverte1) {
+    uneBrasserie, boolean ouverte1) { //fonction pour créer une bière
     Nom = unNom;
     degreAlcool = unDegre;
     brasserie = uneBrasserie;
     ouverte=ouverte1;
     }
     @Override
-    public String toString() {
+    public String toString() {//permet d'afficher également l'étiquette de la bière mais plus synthétiquement
     String chaine_a_retourner;
-    chaine_a_retourner = Nom + " (" + degreAlcool + " degrés)Ouverte ? "; 
+    chaine_a_retourner = Nom + " (" + degreAlcool + " degrés) Ouverte ? "; 
     if (ouverte == true ) chaine_a_retourner += "oui" ;
     else chaine_a_retourner += "non" ;
     return chaine_a_retourner ;
 }
     
     
-}
-    public boolean Décapsuler (){
-    if (ouverte == false) {
-        ouverte =true ;
+
+    public boolean Décapsuler (){//fonction décapsuler
+    if (ouverte == false) {//si pas ouverte
+        ouverte = true ;//On ouvre te on affiche que la bière a été ouverte
         return ouverte;
     }
-    else {
-        System.out.println("erreur, la bière est déjà ouverte");
-        ouverte = false;
+    else {//sinon
+        System.out.println("erreur, la bière est déjà ouverte");//message erreur
+        ouverte = false;//bière déjà ouverte donc on a pas eu besoin de l'ouvrir
         return ouverte ;
-    }
-    
+}
     }
 }
+
 
 
                 
--- TP2_Bières_BARISIEN/src/tp2_bières_barisien/TP2_Bières_BARISIEN.java
+++ TP2_Bières_BARISIEN/src/tp2_bières_barisien/TP2_Bières_BARISIEN.java
@@ -14,12 +14,12 @@ public class TP2_Bières_BARISIEN {
      */
     public static void main(String[] args) {
         
-        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson", false) ;
-        //uneBiere.Nom = "Cuvee des trolls";
+        BouteilleBiere uneBiere = new BouteilleBiere("Cuvée des trolls", 7.0, "Dubuisson", false) ; //appel de la fonction pour créer une bière
+        //uneBiere.Nom = "Cuvee des trolls"; //cette ligne et les 3 suivantes permettaient de définir les premières bières, avec toutes les infos en colonnes
         //uneBiere.degreAlcool = 7.0 ;
         //uneBiere.brasserie = "Dubuisson";
         //uneBiere.ouverte = false ;   
-        System.out.println(uneBiere) ;
+        System.out.println(uneBiere) ; //Affichage de la bière en ligne et avec quelques différences  : les degrés sont entre parenthèses
 
 
        // uneBiere.lireEtiquette();
@@ -40,7 +40,8 @@ public class TP2_Bières_BARISIEN {
         //BiereHeineken.lireEtiquette();
         
         BouteilleBiere BiereViolette = new BouteilleBiere("La Violette", 4.7, "A Annecy", false) ;
-        System.out.println(BiereViolette) ;
+        BiereViolette.Décapsuler();//appel de la fonction décapsuler
+        System.out.println(BiereViolette) ;//affichage
         BouteilleBiere BiereBlanche = new BouteilleBiere("La 1664 Blanche", 4.9, "Chez Cath", false) ;
         System.out.println(BiereBlanche) ;
    }

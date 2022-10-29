8e227de2ecec4f03aec8b2450fd584b04e1ae8b8
marga <margaux.barisien@epfedu.fr>
2022-10-29 17:46:14 +0200

    menu complet + commentaires

--- TP2_convertisseurObjet_BARISIEN/src/tp2_convertisseurobjet_barisien/Convertisseur.java
+++ TP2_convertisseurObjet_BARISIEN/src/tp2_convertisseurobjet_barisien/Convertisseur.java
@@ -1,7 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+/*BARISIEN Margaux
+*TDB 28/10/2022
+*/
 package tp2_convertisseurobjet_barisien;
 
 /**
@@ -11,8 +10,8 @@ package tp2_convertisseurobjet_barisien;
 
 public class Convertisseur { 
        int nbConversions; 
-    public Convertisseur (){
-        nbConversions = 0;
+    public Convertisseur (){//pour calculer nb conversions 
+        nbConversions = 0;// initialisation à 0
     }
     @Override
     public String toString () {
@@ -22,27 +21,27 @@ public class Convertisseur {
     
     
 
-public double CelciusversKelvin (double celcius){ 
-    double kelvin ;
-    kelvin = celcius + 273.15;
-    return kelvin;
+public double CelciusversKelvin (double celcius){ // création de toutes les fonctions allant de celcius vers kelvin ou encore de farenheit vers celcius
+    double kelvin ;// création du type de la variable de sortie
+    kelvin = celcius + 273.15;//le rapport entre kelvin et celcius
+    return kelvin;//renvoie la valeur que l'utilisateur entrera comme des celcius mais en kelvin
 }
-public double KelvinversCelcius (double kelvin){ 
+public double KelvinversCelcius (double kelvin){ //même principe
     double celcius ;
     celcius = kelvin - 273.15;
     return kelvin;
 }
-public double FarenheitversCelcius (double farenheit){ 
+public double FarenheitversCelcius (double farenheit){ //même principe
     double celcius ;
     celcius = (farenheit - 32)/1.8 ;
     return celcius;
 }
-public double CelciusversFarenheit (double celcius){ 
+public double CelciusversFarenheit (double celcius){ //même principe
     double farenheit ;
     farenheit = (celcius*1.8) +32 ;
     return farenheit;
 }
-public double KelvinversFarenheit (double kelvin){ 
+public double KelvinversFarenheit (double kelvin){ // on utilise les fonctions précédentes : pour faire kelvin farenheit on fait kelvincelcius-celciusfarenheit
     double farenheit ;
     farenheit = CelciusversFarenheit(KelvinversCelcius(kelvin));   
     return farenheit;
--- TP2_convertisseurObjet_BARISIEN/src/tp2_convertisseurobjet_barisien/TP2_convertisseurObjet_BARISIEN.java
+++ TP2_convertisseurObjet_BARISIEN/src/tp2_convertisseurobjet_barisien/TP2_convertisseurObjet_BARISIEN.java
@@ -19,30 +19,30 @@ public class TP2_convertisseurObjet_BARISIEN {
      */
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        double b = sc.nextDouble();
+        Scanner sc = new Scanner (System.in);//nécessaire pour demander une saisi à l'utilisateur
+        double b = sc.nextDouble();// saisi de l'utilisateur
         System.out.println(b);
-        double k = b + 273.15 ;
-        System.out.println(k); 
+        double k = b + 273.15 ;// calcul
+        System.out.println(k); //affichage k
     
         
-        double test = sc.nextDouble();
-        Convertisseur c = new Convertisseur ();
+        double test = sc.nextDouble();//test 
+        Convertisseur c = new Convertisseur ();// c devient convertisseur fonction donc permet d'être utilisé ci-dessous
         double resultat = c.CelciusversFarenheit(test);
         System.out.println(resultat);
     
-        System.out.println("Saisissez une valeur");
-        double valeur = sc.nextDouble();
-        System.out.println("Saisissez la conversion que vous souhaiter effectuer : \n 1) De Celcius vers Kelvin \n 2) De Kelvin vers Celcius \n 3) Farenheit vers Celcius \n 4) Celcius vers Farenheit \n 5) De Kelvin vers Farenheit \n 6) De Farenheit vers Kelvin");
-        double conversion = sc.nextDouble();
-        double resultatfinal;
-        int nbConversions = 0;
-        if (conversion == 1){
+        System.out.println("Saisissez une valeur");//affichage début menu
+        double valeur = sc.nextDouble();//saisi de l'utilisateur
+        System.out.println("Saisissez la conversion que vous souhaiter effectuer : \n 1) De Celcius vers Kelvin \n 2) De Kelvin vers Celcius \n 3) Farenheit vers Celcius \n 4) Celcius vers Farenheit \n 5) De Kelvin vers Farenheit \n 6) De Farenheit vers Kelvin");//affichage suite menu
+        double conversion = sc.nextDouble();//saisi utilisateur
+        double resultatfinal;//création de la variable resultat final de type double
+        int nbConversions = 0;//initialisation de la variable nb conversions
+        if (conversion == 1){//test pour savoir quel genre de changement de température l'utilisateur souhaite
             resultatfinal = c.CelciusversKelvin(valeur);
             System.out.println(valeur + "degres Celcius est egal à " + resultatfinal + "degres Kelvin");
-            nbConversions=nbConversions + 1;
+            nbConversions=nbConversions + 1;// on rajoute +1 pour dire qu'une conversion a été effectuée
         }
-        if (conversion == 2){
+        if (conversion == 2){//même principe 
             resultatfinal = c.KelvinversCelcius(valeur);
             System.out.println(valeur + "degres Kelvin est egal à " + resultatfinal + "degres Celcius");
             nbConversions=nbConversions + 1;
@@ -67,7 +67,7 @@ public class TP2_convertisseurObjet_BARISIEN {
             System.out.println(valeur + "degres Farenheit est egal à " + resultatfinal + "degres Kelvin");
             nbConversions=nbConversions + 1;
         }
-        System.out.println("Le nombre de conversions est égal à "+ nbConversions); 
+        System.out.println("Le nombre de conversions est égal à "+ nbConversions); //affichage nb conversions
     }
     
 }

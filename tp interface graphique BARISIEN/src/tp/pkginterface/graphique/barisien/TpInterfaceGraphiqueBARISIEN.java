8b41b03a28ac3d0d185e2b7caa78c48a7fed05d2
marga <margaux.barisien@epfedu.fr>
2022-11-07 11:38:46 +0100

    création projet interface graphique

---
+++ tp interface graphique BARISIEN/src/tp/pkginterface/graphique/barisien/TpInterfaceGraphiqueBARISIEN.java
@@ -0,0 +1,20 @@
+/*BARISIEN Margaux
+*TDB 
+*07/11/2022
+ */
+package tp.pkginterface.graphique.barisien;
+
+/**
+ *
+ * @author marga
+ */
+public class TpInterfaceGraphiqueBARISIEN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

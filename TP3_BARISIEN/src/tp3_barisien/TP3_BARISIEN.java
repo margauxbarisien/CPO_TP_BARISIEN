4c720dc75aebe6daf9f4fef7856935b92421bbaa
marga <margaux.barisien@epfedu.fr>
2022-11-14 10:14:00 +0100

    épées + batons

--- TP3_BARISIEN/src/tp3_barisien/TP3_BARISIEN.java
+++ TP3_BARISIEN/src/tp3_barisien/TP3_BARISIEN.java
@@ -13,6 +13,10 @@ public class TP3_BARISIEN {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Epee Excalibur = new Epee ("Excalibur",7,5);
+        Epee Durandal = new Epee ("Excalibur",4,7);
+        Baton Chene = new Baton ("Chêne", 4,5);
+        Baton Charme = new Baton ("Charme", 5,6);
         // TODO code application logic here
     }
     
